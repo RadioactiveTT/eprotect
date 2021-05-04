3ab7c9d8ee0892ce62ef6024e40b6943fc694b37
RadioactiveTT <atlsrhmf@gmail.com>
2021-05-05 00:45:27 +0300

    11111

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,29 @@
-$(document).ready(function(){
+$(document).ready(function () {
   $('.cta__slider').slick({
-  fade: true,
-  dots:true,
-  autoplay: true,
-  autoplaySpeed: 4000,
+    fade: true,
+    dots: true,
+    autoplay: true,
+    autoplaySpeed: 4000,
+    responsive: [
+      {
+        breakpoint: 1200,
+        settings: {
+          dots: false
+        }
+      },
+      {
+        breakpoint: 756,
+        settings: {
+          arrows: false,
+          dots: false
+        }
+      }
+    ]
   });
 
 });
 
-$('.burger').on('click', function(){
-console.log('click');
-$('.header').toggleClass('mobile')
+$('.burger').on('click', function () {
+  console.log('click');
+  $('.header').toggleClass('mobile')
 });
